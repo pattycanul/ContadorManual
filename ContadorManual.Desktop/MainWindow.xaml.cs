0d68ba24341819ea5e2a528ee272fef1de55e7cd
Patricia del Jasmin Canul Canul <20470105@campeche.tecnm.mx>
2021-09-21 01:42:43 -0500

    Agrega el manejo de eventos de los botones

--- ContadorManual.Desktop/MainWindow.xaml.cs
+++ ContadorManual.Desktop/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace ContadorManual.Desktop
 {
@@ -20,9 +7,28 @@ namespace ContadorManual.Desktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int _Conteo;
         public MainWindow()
         {
             InitializeComponent();
+            _Conteo = 0;
+        }
+
+        private void ContarButton_Click(object sender, RoutedEventArgs e)
+        {
+            _Conteo++;
+            ConteoLabel.Content = _Conteo;
+        }
+
+        private void CalcularButton_Click(object sender, RoutedEventArgs e)
+        {
+            _Conteo = 0;
+            ConteoLabel.Content = _Conteo;
+        }
+
+        private void SalirButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
